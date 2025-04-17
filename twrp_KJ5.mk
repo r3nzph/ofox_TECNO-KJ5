3ec58b127b31a44a9ce37b43702c2bb514b74a5d
r3nzph <abarracosorenznino@gmail.com>
2025-04-18 02:46:39 +0800

    Update twrp_KJ5.mk

--- twrp_KJ5.mk
+++ twrp_KJ5.mk
@@ -1,18 +1,9 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
-
 
 # Inherit from KJ5 device
 $(call inherit-product, device/tecno/KJ5/device.mk)
@@ -24,4 +15,4 @@ PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO KJ5
 PRODUCT_MANUFACTURER := tecno
 
-PRODUCT_GMS_CLIENTID_BASE := android-tecno
+PRODUCT_GMS_CLIENTID_BASE := android-transsion
